cb6b1929f3a463d6f7dce7cfcccc0cd326c55467
Levy <kipkoechlevy096@gmail.com>
2023-01-28 19:42:03 +0300

    created authentication files and their functionality

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import { SiMoleculer } from "react-icons/si";
 import { useNavigate } from 'react-router-dom';
 import About from "./About";
 import Display from "../display/Display";
 import {Routes, Route} from "react-router-dom";
+import { Login } from "../Authentication/login";
+import { Register } from "../Authentication/registration";
 
 
 function Home(){
+   
+    const [currentForm, setCurrentForm] = useState ('Login')
+
+     const toggleForm =(forName)=>{
+             setCurrentForm(forName)
+     } 
     return (
         <div>
+
+            {
+                currentForm === "Login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+            }
             
             <input type="checkbox" id="active"/>
             <label for="active" className="menu-btn">
